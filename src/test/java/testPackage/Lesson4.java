31ff84856b92c65fa1c025920f2e89881ea9f26e
Tamara <t.talanceva@yandex.ru>
2025-01-03 17:05:29 +0700

    deleted comments

--- src/test/java/testPackage/Lesson4.java
+++ src/test/java/testPackage/Lesson4.java
@@ -38,23 +38,15 @@ public class Lesson4 {
 
     @Test
     void testProjectSelenide() {
-        //открыть главную страницу Гитхаба
         open("https://github.com");
-        //ввести в поле поиска selenide и нажать enter
         $(".search-input").click();
         $("#query-builder-test").setValue("selenide").pressEnter();
-        //кликнуть на репозиторий selenide/selenide
         $("a[href='/selenide/selenide']").click();
-        //проверить, что мы в репе selenide/selenide
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
-        //перейти на страницу wiki
         $("#wiki-tab").click();
-        //Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-        //Открыть страницу SoftAssertions
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        //Проверить, что внутри есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
         $(".markdown-body").shouldHave(Condition.text(containsCode));
     }
